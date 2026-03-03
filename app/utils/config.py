63de53b52a534a168d5782f12b7918879acfed9c
Ingrid Xisto <ingridxisto@gmail.com>
2026-03-03 09:41:18 -0300

    Ajuste leitura API key para Streamlit Cloud

--- app/utils/config.py
+++ app/utils/config.py
@@ -4,8 +4,7 @@ import streamlit as st
 
 load_dotenv()
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
-
-# Se não encontrar no .env, pega do secrets da Streamlit
-if not API_KEY:
+if "OPENWEATHER_API_KEY" in st.secrets:
     API_KEY = st.secrets["OPENWEATHER_API_KEY"]
+else:
+    API_KEY = os.getenv("OPENWEATHER_API_KEY")
